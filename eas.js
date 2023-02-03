06223f0487a9c1b1fff889499e5dabdaa652ca7b
Johannes <J-Bird@gmx.de>
2023-02-03 20:53:56 +0100

    Create Buttons. Next: add function to button

--- eas.js
+++ eas.js
@@ -1,18 +1,30 @@
 //Declare grid size 16x16
 const gridCount = 16;
+const gridElemSize = 1;
+
 
 //declare and style container
 const canvas = document.getElementById('container');
-canvas.style.cssText = 'display: flex; flex-wrap: wrap;';
-canvas.style.cssText += `width: calc(2rem * ${gridCount})`;
+canvas.style.cssText = 'display: flex; flex-wrap: wrap; max-width: 960px; max-height: 960px; margin: auto; border: 1px solid darkgrey;';
+canvas.style.cssText += `width: calc((${gridElemSize}*1rem) * ${gridCount})`;
 
-function row(gridCount) {
+//Create Grid
+function makeGrid(gridCount) {
     for(let i = 1; i <= (gridCount * gridCount); i++) {
-        const gridRow = document.createElement('div');
-        gridRow.classList.add('row');
-        gridRow.style.cssText = 'height: 2rem; width: 2rem; background-color: red; border: 1px solid lightgrey';
-        canvas.appendChild(gridRow);
+        const gridElem = document.createElement('div');
+        gridElem.classList.add('item');
+        gridElem.style.cssText = `height: calc(${gridElemSize}*1rem); width: calc(${gridElemSize}*1rem); background-color: white; border: 1px solid darkgrey`;
+        gridElem.addEventListener('mouseleave', () => {
+            gridElem.style.cssText += 'background-color: black;';
+        })
+        canvas.appendChild(gridElem);
     }
 }
 
-row(gridCount);
+//Call "Create grid" function
+makeGrid(gridCount);
+
+//Let colors change on hover
+
+
+
